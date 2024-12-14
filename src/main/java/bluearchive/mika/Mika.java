7f25124b9401274b9e46eb525c7785f3cb41a5ec
slbz233 <slbz233@163.com>
2024-12-14 09:10:54 +0800

    Add files via upload

---
+++ src/main/java/bluearchive/mika/Mika.java
@@ -0,0 +1,17 @@
+package bluearchive.mika;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mika extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
